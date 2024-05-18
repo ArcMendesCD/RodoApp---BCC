78395fadfa15aff667599dc1cf814480513ae7a4
ArcMendesCD <andreircmendes@gmail.com>
2024-05-17 23:12:14 -0300

    Primeiro Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RodoApp',
+  webDir: 'www'
+};
+
+export default config;
