881c986111123cb573d1998c8ceab30878847200
laura yumi <laurayumi30@gmail.com>
2024-05-30 17:47:14 -0300

    ion-tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
   constructor() {}
+
+  // curveTab(event: Event) {
+  //   const buttons = document.querySelectorAll('ion-tab-button');
+  //   buttons.forEach(button => button.classList.remove('curved'));
+  //   const clickedButton = event.currentTarget as HTMLElement;
+  //   clickedButton.classList.add('curved');
+  // }
 }
+
