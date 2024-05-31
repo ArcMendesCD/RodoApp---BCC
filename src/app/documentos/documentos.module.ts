c6dcb5d47906c76f2795f2629890126e0fbbe42c
Giovana Castro <128002398+GiCCastro@users.noreply.github.com>
2024-05-31 16:10:46 +0000

    alterações na página documentos

--- src/app/documentos/documentos.module.ts
+++ src/app/documentos/documentos.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-import { DocumentosPageRoutingModule } from './documentos-routing.module';
-
-import { DocumentosPage } from './documentos.page';
+import { DocumentosPageRoutingModule } from './documentos-routing.module'; // Importa o módulo de roteamento da página de documentos
+import { DocumentosPage } from './documentos.page'; // Importa o componente da página de documentos
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    DocumentosPageRoutingModule
+    CommonModule, // Importa CommonModule para funcionalidades comuns do Angular
+    FormsModule,  // Importa FormsModule para funcionalidades de formulários
+    IonicModule,  // Importa IonicModule para componentes do Ionic
+    DocumentosPageRoutingModule // Importa o módulo de roteamento da página de documentos
   ],
-  declarations: [DocumentosPage]
+  declarations: [DocumentosPage] // Declara o componente DocumentosPage
 })
-export class DocumentosPageModule {}
+export class DocumentosPageModule { }
--- src/app/documentos/documentos.page.ts
+++ src/app/documentos/documentos.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-documentos',
   templateUrl: './documentos.page.html',
   styleUrls: ['./documentos.page.scss'],
 })
-export class DocumentosPage implements OnInit {
+export class DocumentosPage {
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  triggerFileInput() {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    if (fileInput) {
+      fileInput.click();
+    }
   }
 
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      // Lógica para lidar com o arquivo selecionado
+      console.log('Arquivo selecionado:', file.name);
+    }
+  }
 }
