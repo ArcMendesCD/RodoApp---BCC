c1a3b5754202630cfef31cf94044c4805bcf9594
laura yumi <laurayumi30@gmail.com>
2024-06-02 19:07:01 -0300

    feat: creating service logic to show the db informations in the frete page

--- src/app/frete/frete.page.ts
+++ src/app/frete/frete.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from '../services/item.service';
 
 @Component({
   selector: 'app-frete',
@@ -7,15 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FretePage implements OnInit {
 
-  constructor() { }
+  item: any;
+  public segment: string = "list";
+  users: any[] = [];
+  public userNames: string[] = [];
+  private apiUrl = 'http://168.121.216.20/users.php';
+
+  constructor(private route: ActivatedRoute, private itemService: ItemService, private http: HttpClient) { }
 
   ngOnInit() {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+      //ALL NAMES
+    // this.itemService.getUsers().subscribe(response => {
+    //   this.users = response;
+    //   this.extractUserNames();
+    // });
+
+      // NAME 0
+    // this.itemService.getUsers().subscribe(response => {
+    //   if (response.length > 0) {
+    //     this.userNames = response[0].first_name;
+    //   }
+    // });
+
+      // ONLY 'LAURA'
+    this.itemService.getUsers().subscribe(response => {
+      this.users = response.filter(user => user.last_name === 'Yumi');
+    });
   }
-  // PEGAR DO BANCO
-  public segment: string = "list";
-  public arr = new Array(5);
+  //ALL NAMES
+  // extractUserNames() {
+  //   this.userNames = this.users.map(user => user.first_name);
+  // }
+
+
 
   segmentChanged(ev: any) {
     this.segment = ev.detail.value;
+    this.itemService.loadUsers();
   }
+
 }
