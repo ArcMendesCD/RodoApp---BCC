2796042334da770264c6babc7cd8fe58d3610287
Giovana Castro <128002398+GiCCastro@users.noreply.github.com>
2024-06-04 22:28:39 +0000

    atualizações

--- src/app/documentos/documentos.page.ts
+++ src/app/documentos/documentos.page.ts
@@ -1,6 +1,7 @@
 // documentos.page.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 
 
@@ -11,7 +12,9 @@ import { Router } from '@angular/router';
 })
 export class DocumentosPage {
 
-  constructor(private router: Router) { }
+  // constructor(private router: Router) { }
+  constructor(private navCtrl: NavController){}
+
 
   onFileSelected(tipoDocumento: string, event: any) {
     const file: File = event.target.files[0];
@@ -19,6 +22,8 @@ export class DocumentosPage {
      
     }
   }
+
+
 
   triggerFileInput(idInput: string) {
     const inputElement = document.getElementById(idInput) as HTMLInputElement;
@@ -28,10 +33,10 @@ export class DocumentosPage {
   }
 
   navegarParaNotaFiscal() {
-    this.router.navigate(['/nota-fiscal']);
+    this.navCtrl.navigateForward(['/nota-fiscal']);
   }
 
   navegarParaNotaCarga() {
-    this.router.navigate(['/carga']);
+    this.navCtrl.navigateForward(['/carga']);
   }
 }
