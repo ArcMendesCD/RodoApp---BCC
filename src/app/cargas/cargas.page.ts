1b011f245c530bac78197d2e6012efe70ab796d5
Giovana Castro <128002398+GiCCastro@users.noreply.github.com>
2024-06-05 15:34:34 +0000

    modificações na página cargas

--- src/app/cargas/cargas.page.ts
+++ src/app/cargas/cargas.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cargas',
   templateUrl: './cargas.page.html',
   styleUrls: ['./cargas.page.scss'],
 })
-export class CargasPage implements OnInit {
+export class CargasPage {
+  documento: any; // Adicione a lógica necessária para inicializar este documento
 
   constructor() { }
 
-  ngOnInit() {
+  downloadArquivo(url: string) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = this.documento.nomeArquivo;
+    a.click();
   }
-
 }
--- src/app/documentos/documentos.page.ts
+++ src/app/documentos/documentos.page.ts
@@ -1,29 +1,75 @@
-// documentos.page.ts
+// // documentos.page.ts
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { NavController } from '@ionic/angular';
+
+
+
+// @Component({
+//   selector: 'app-documentos',
+//   templateUrl: './documentos.page.html',
+//   styleUrls: ['./documentos.page.scss'],
+// })
+// export class DocumentosPage {
+
+//   // constructor(private router: Router) { }
+//   constructor(private navCtrl: NavController){}
+
+
+//   onFileSelected(tipoDocumento: string, event: any) {
+//     const file: File = event.target.files[0];
+//     if (file) {
+     
+//     }
+//   }
+
+
+
+//   triggerFileInput(idInput: string) {
+//     const inputElement = document.getElementById(idInput) as HTMLInputElement;
+//     if (inputElement) {
+//       inputElement.click();
+//     }
+//   }
+
+//   navegarParaNotaFiscal() {
+//     this.navCtrl.navigateForward(['/nota-fiscal']);
+//   }
+
+//   navegarParaNotaCarga() {
+//     this.navCtrl.navigateForward(['/cargas']);
+//   }
+// }
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
-
-
 @Component({
-  selector: 'app-documentos',
-  templateUrl: './documentos.page.html',
-  styleUrls: ['./documentos.page.scss'],
+
+    selector: 'app-documentos',
+    templateUrl: './documentos.page.html',
+    styleUrls: ['./documentos.page.scss'],
+  
 })
 export class DocumentosPage {
+  documento: any;
 
-  // constructor(private router: Router) { }
-  constructor(private navCtrl: NavController){}
-
+  //  constructor(private router: Router) { }
+    constructor(private navCtrl: NavController){}
+  
 
   onFileSelected(tipoDocumento: string, event: any) {
-    const file: File = event.target.files[0];
+    const file = event.target.files[0];
     if (file) {
-     
+      this.documento = {
+        nomeArquivo: file.name,
+        dataUpload: new Date(),
+        tamanhoArquivo: (file.size / 1024).toFixed(2), // tamanho em KB
+        tipoDocumento: tipoDocumento,
+        url: URL.createObjectURL(file) // criando uma URL para visualizar o arquivo
+      };
     }
   }
-
-
 
   triggerFileInput(idInput: string) {
     const inputElement = document.getElementById(idInput) as HTMLInputElement;
@@ -32,7 +78,14 @@ export class DocumentosPage {
     }
   }
 
-  navegarParaNotaFiscal() {
+  downloadArquivo(url: string) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = this.documento.nomeArquivo;
+    a.click();
+  }
+
+   navegarParaNotaFiscal() {
     this.navCtrl.navigateForward(['/nota-fiscal']);
   }
 
