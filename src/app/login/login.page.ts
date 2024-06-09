cc2dc06d7c3469a5aeaa8c878aa62164e89f2067
LYO <142507365+LauraYumiOsawa@users.noreply.github.com>
2024-06-09 20:06:27 -0300

    login - coemco

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from '../services/item.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private itemService: ItemService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.itemService.loadData();
   }
 
+
+
+  // if (this.uname.length === 0 || this.password.length === 0) {
+  //   this.alertMessage = "Campos obrigatórios não podem estar em branco!";
+  //   return;
+  // }
 }
